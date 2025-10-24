cf880f4bcd624658618362c56846063e411b9757
ticianosilva78-coder <ticianosilva78@gmail.com>
2025-10-23 21:59:35 -0300

    inicio projeto

---
+++ sistema-estoque/src/test/java/com/estoque/sistemaestoque/SistemaEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estoque.sistemaestoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
